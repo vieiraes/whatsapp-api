51c0ca28a0f8ccfb31f97ddd17a9b1189460a25e
Bruno Vieira <vieira.es@gmail.com>
2024-11-18 23:51:56 -0300

    feat:added export to svg and png

--- src/controllers/whatsapp.controller.ts
+++ src/controllers/whatsapp.controller.ts
@@ -1,6 +1,13 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 import { WhatsAppService } from '../services/whatsapp.service';
+import QRCode from 'qrcode';
 
+
+
+
+interface Query {
+    output?: string; // Define que 'output' é um parâmetro opcional
+}
 export class WhatsAppController {
     private whatsappService: WhatsAppService;
 
@@ -13,11 +20,27 @@ export class WhatsAppController {
         return reply.send({ message: 'WhatsApp client initialized' });
     }
 
-    async getQR(req: FastifyRequest, reply: FastifyReply) {
-        const qr = await this.whatsappService.getQR();
-        return reply.send({ qr });
-    }
+    async getQR(req: FastifyRequest<{ Querystring: Query }>, reply: FastifyReply) {
+        const qrData = await this.whatsappService.getQR(); // Obtenha o QR code em formato de string
 
+        // Obtém o parâmetro de consulta 'output'
+        const outputFormat = req.query.output || 'png'; // Default para 'png' se não fornecido
+
+        try {
+            let qrImage;
+
+            // Gera a imagem em formato especificado
+            if (outputFormat === 'svg') {
+                qrImage = await QRCode.toString(qrData.qr, { type: 'svg' });
+                reply.type('image/svg+xml').send(qrImage);
+            } else {
+                qrImage = await QRCode.toDataURL(qrData.qr, { type: 'image/png' });
+                reply.type('image/png').send(Buffer.from(qrImage.split(',')[1], 'base64'));
+            }
+        } catch (error) {
+            reply.status(500).send({ error: 'Error generating QR code' });
+        }
+    }
     async sendMessage(req: FastifyRequest<{ Body: { to: string, message: string } }>, reply: FastifyReply) {
         const { to, message } = req.body;
         const result = await this.whatsappService.sendMessage(to, message);
